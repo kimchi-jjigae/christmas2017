11161109209370ed1e83fa2438ad7c64342ea94b
Tobias Widlund <widlundtobias@gmail.com>
2016-12-23 01:40:58 +0100

    removed comments

--- src/entitystates/states/playerstates.cpp
+++ src/entitystates/states/playerstates.cpp
@@ -19,9 +19,6 @@ void registerPlayerStates(GameData& gameData)
                 Transitions
                 {
                     { StateAction("bored"_hash), StateHash("wander"_hash) },
-                    //{ StateAction("slow_down"_hash), StateHash("walk"_hash) },
-                    //{ StateAction("turn"_hash), StateHash("turn_slide"_hash) },
-                    //{ StateAction("jump"_hash), StateHash("jump"_hash) },
                 },
                 //any state can have zero or more executors
                 Executors
@@ -45,8 +42,6 @@ void registerPlayerStates(GameData& gameData)
                                 context.emitTransition = "bored"_hash;
                         },
                     },
-                    //standard sprite animation manipulating executor that plays run animation
-                    //spriteAnimateExecutor({0, 36}, {12, 14}, 3, 10, Animation::Loop),
                 },
             }
         },
@@ -60,9 +55,6 @@ void registerPlayerStates(GameData& gameData)
                 Transitions
                 {
                     { StateAction("_next"_hash), StateHash("idle"_hash) },
-                    //{ StateAction("slow_down"_hash), StateHash("walk"_hash) },
-                    //{ StateAction("turn"_hash), StateHash("turn_slide"_hash) },
-                    //{ StateAction("jump"_hash), StateHash("jump"_hash) },
                 },
                 //any state can have zero or more executors
                 Executors
@@ -95,58 +87,8 @@ void registerPlayerStates(GameData& gameData)
                             }
                         },
                     },
-                    //standard sprite animation manipulating executor that plays run animation
-                    //spriteAnimateExecutor({0, 36}, {12, 14}, 3, 10, Animation::Loop),
                 },
             }
         },
-        //our jump state, which is after the player pressed jump when being on ground
-        //{"jump"_hash,
-        //    EntityState
-        //    {
-        //        //duration of the state
-        //        Duration(60),
-        //        //state actions that lead to transitioning to another state
-        //        Transitions
-        //        {
-        //            { StateAction("_next"_hash), StateHash("fall"_hash) },
-        //            { StateAction("stop"_hash), StateHash("fall"_hash) },
-        //        },
-        //        //any state can have zero or more executors
-        //        Executors
-        //        {
-        //            //this executor makes us go upwards and excerts some air control
-        //            Executor
-        //            {
-        //                "movement_controller",
-        //                everyNthFrame(1, 0),
-        //                [] (StateContext& context, GameData& data)
-        //                {
-        //                    //int32_t objectId = context.entityId;
-
-        //                    //glm::vec2& velocity = get(objectId, data.tVelocity);
-        //                    //velocity.y = -5.0f;
-        //                    //velocity.x = get(objectId, data.tDirections);
-        //                },
-        //            },
-        //            //this executor makes sure we can stop falling
-        //            Executor
-        //            {
-        //                //condition
-        //                "fall_timer",
-        //                everyNthFrame(1, 0),
-        //                [] (StateContext& context, GameData& data)
-        //                {
-        //                    //PlayerAction currentAction = data.currentPlayerAction;
-
-        //                    //if(currentAction != Action::Jump)
-        //                    //    context.emitTransition = "stop"_hash;
-        //                },
-        //            },
-        //            //standard sprite animation manipulating executor that plays run animation
-        //            //spriteAnimateExecutor({0, 36}, {12, 14}, 3, 10, Animation::ForwardStop),
-        //        },
-        //    }
-        //},
     }, gameData);
 }
