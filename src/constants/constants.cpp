fe393466455edc908178b06f5e844aef4c95a83c
Kim Wong <kimspindel@gmail.com>
2017-10-08 19:22:33 +0200

    Change bg colour

--- src/constants/constants.cpp
+++ src/constants/constants.cpp
@@ -14,7 +14,7 @@ std::unique_ptr<Constants> makeConstants()
     result.world = std::make_unique<constants::World>();
 
     //world
-    result.world->voidColor = {50, 0, 10, 255};
+    result.world->voidColor = spr::Color(0x43556b);
     result.world->spatialStorageSize =  {1024.0f};
 
     return std::make_unique<Constants>(std::move(result));
